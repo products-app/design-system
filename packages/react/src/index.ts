ad69feeeb3f61ca49be8bf186d9e8ea9ed6e2d18
leticiabernardo <leticia.bernardo@mercadolivre.com>
2023-10-04 16:08:48 -0300

    feat: add react packages to build a first component

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@leticiabernardo/common'
-
-console.log('ok', colors)
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+import { colors } from '@leticiabernardo/common'
+
+export function App() {
+  return <h1 style={{ color: colors.base300 }}>Hello World</h1>
+}
