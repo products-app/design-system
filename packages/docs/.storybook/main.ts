d107b4b752d7ce2ebc85a93ce25d95aa9c1a6aa5
leticiabernardo <leticia.bernardo@mercadolivre.com>
2023-10-04 17:52:26 -0300

    feat: configure storybook to display the components

---
+++ packages/docs/.storybook/main.ts
@@ -0,0 +1,30 @@
+import { dirname, join } from "path";
+module.exports = {
+  "stories": [
+    "../src/stories/**/*.stories.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+
+  "addons": [
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions")
+  ],
+
+  "framework": {
+    name: getAbsolutePath("@storybook/react-vite"),
+    options: {}
+  },
+
+  "features": {
+    "storyStoreV7": true
+  },
+
+  docs: {
+    autodocs: true
+  }
+}
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,12 +1,37 @@
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
-  background: '$base700',
-  padding: '$4',
-  height: '$16',
+  background: '$base500',
+  border: 'none',
+  borderRadius: '8px',
+  color: '$white',
+  fontSize: '$md',
+  cursor: 'pointer',
+  fontWeight: '$bold',
+
+  '&:hover': {
+    background: '$base700',
+  },
+
+  variants: {
+    size: {
+      small: {
+        fontSize: '$14',
+        padding: '$2 $4',
+        height: '$14',
+      },
+      large: {
+        fontSize: '$16',
+        padding: '$3 $6',
+        height: '$16',
+      },
+    },
+  },
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
-export function App() {
-  return <Button>Hello</Button>
-}
+export type ButtonProps = ComponentProps<typeof Button>
