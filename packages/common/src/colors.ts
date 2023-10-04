48627e57d86c5af3f1769dcf45c39d3b4f5c3d31
leticiabernardo <leticia.bernardo@mercadolivre.com>
2023-10-04 15:07:36 -0300

    feat: add configuration of common types

---
+++ packages/common/src/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  white: '#FFF',
+  black: '#000',
+
+  gray100: '#CCCED0',
+  gray200: '#B0B5B8',
+  gray400: '#7C848B',
+  gray500: '#505059',
+  gray600: '#323238',
+  gray700: '#29292E',
+  gray800: '#202024',
+  gray900: '#121214',
+
+  base300: '#00c4b4',
+  base500: '#04a19a',
+  base700: '#038786',
+  base900: '#005457',
+}
