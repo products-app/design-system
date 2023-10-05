7be4ddfecac501562b303fa5cb529254d9579293
leticiabernardo <leticia.bernardo@mercadolivre.com>
2023-10-05 18:24:13 -0300

    feat: add checkout component

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Card, Text, Checkbox, CheckboxProps } from '@leticiabernardo/react'
+
+export default {
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Card
+          as="label"
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          {Story()}
+          <Text size="sm">Accept terms of use</Text>
+        </Card>
+      )
+    },
+  ],
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {}
