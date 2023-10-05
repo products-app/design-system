123cbd1cd83da1f15790c7e2542f1aee344e4ea5
leticiabernardo <leticia.bernardo@mercadolivre.com>
2023-10-05 18:00:50 -0300

    feat: add text input component

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Card, Text, TextInput, TextInputProps } from '@leticiabernardo/react'
+
+export default {
+  title: 'Form/Text Input',
+  component: TextInput,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Card
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">Text</Text>
+          {Story()}
+        </Card>
+      )
+    },
+  ],
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Type your name',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'R$',
+  },
+}
